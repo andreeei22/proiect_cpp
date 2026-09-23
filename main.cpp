3b9e2c7a887daff2004cd60bf288c58eb7f7e4f7
andreeei22 <chiruandreicristian22@gmail.com>
2026-09-24 00:05:07 +0300

    Implementat structura modulara main.cpp math.h si math.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-#include<iostream> int main(){std::cout <<"Hello FMI UB!"<<std::endl; return 0;}
+#include"math.h"
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3, b=4;
+    int suma=add(a,b);
+    std::cout<<"Suma dintre a si b este:";
+    std::cout<<suma;
+    return 0;
+}
